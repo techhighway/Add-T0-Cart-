eb2bff5cac2180ee7f8dd8d9c6e68d3bf5735a2d
techhighway <adityab@techhighway.co.in>
2015-11-20 16:42:09 +0530

    To change icon image

--- add_to_cart/__openerp__.py
+++ add_to_cart/__openerp__.py
@@ -15,7 +15,7 @@ OpenERP E-Commerce
 	                    'views/add_to_cart_template_view.xml',
     		          ],
 
-    	'images': ['static/description/add2cart_add_to_cart.png'],	          
+    	'images': ['static/description/icon.png'],	          
     	'installable': True,
     	'application': True,
 }
